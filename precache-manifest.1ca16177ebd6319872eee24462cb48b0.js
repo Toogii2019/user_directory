6db1965d9d8c0e644351e2768524993beab13989
Toogii <dashdava@uw.edu>
2020-11-04 22:43:43 -0800

    Updates

---
+++ precache-manifest.1ca16177ebd6319872eee24462cb48b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1db883e37972aac4b880e82155b9f37",
+    "url": "/user_directory/index.html"
+  },
+  {
+    "revision": "ee113c3be83f47bfac45",
+    "url": "/user_directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3b4a4d41097f2b6e40a2",
+    "url": "/user_directory/static/css/main.378c773b.chunk.css"
+  },
+  {
+    "revision": "ee113c3be83f47bfac45",
+    "url": "/user_directory/static/js/2.01a8d3f3.chunk.js"
+  },
+  {
+    "revision": "3b4a4d41097f2b6e40a2",
+    "url": "/user_directory/static/js/main.e695d224.chunk.js"
+  },
+  {
+    "revision": "d32a57e65b92a9d58c1d",
+    "url": "/user_directory/static/js/runtime~main.93e4edc1.js"
+  }
+]);
